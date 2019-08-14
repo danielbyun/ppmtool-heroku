5cf7f19ec386ccfdc93acb34f17c3e38360aa0e6
danielbyun <dbyun92@gmail.com>
2019-08-14 17:46:05 +0900

    front end from react

---
+++ src/main/resources/static/precache-manifest.2243c8e054f9f7d0fe120e38876b79e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891878725e72bcf74688989cddba93f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ba057129172017a46d",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d43b6a7c13bc09b84ae8",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "78ba057129172017a46d",
+    "url": "/static/js/2.da14f649.chunk.js"
+  },
+  {
+    "revision": "d43b6a7c13bc09b84ae8",
+    "url": "/static/js/main.a131abae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
